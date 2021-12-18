df2472b0168da86e3e80086a76fdc7f02db42d24
fallf <87919117+fallf@users.noreply.github.com>
2021-12-18 10:48:27 -0500

    update

--- asset/css/js/question.js
+++ asset/css/js/question.js
@@ -1,5 +1,33 @@
 const questionsArr = [
-{
+    {
+        question: 'Which of the following is true about variable naming conventions in JavaScript?',
+        choices: ["A - JavaScript variable names must begin with a letter or the underscore character.",
+        "B - JavaScript variable names are case sensitive.",
+        "C - Both of the above.",
+        "D - None of the above."],
+        answer: "C - Both of the Above"
+    },
+    {
+        question:'Can you assign a anonymous function to a variable?',
+        choices:["A - true","B - false" ],
+        answer:"A - true",
+    },
+
+    {
+        question:'Which built-in method returns the character at the specified index?',
+        choices:[],
+        answer:"",
+    },
+    {
+        question:'',
+        choices:[],
+        answer:"",
+    },
+    {
+        question:'',
+        choices:[],
+        answer:"",
+    }
 
 
-}]
+]
