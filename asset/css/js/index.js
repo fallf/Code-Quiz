74fd0d6408fdb00aea3e51fd5d49ab1867177640
fallf <87919117+fallf@users.noreply.github.com>
2021-12-18 15:44:37 -0500

    update time added

--- asset/css/js/index.js
+++ asset/css/js/index.js
@@ -1,4 +1,43 @@
 // logic track time
+var time = 5
+var timerId;
 //start the quiz 
 // determine if the user wrote the right question
 // determine the score as well as 
+
+const questionsEL = document.getElementById("question");
+const timerEL = document.getElementById("timer");
+//var choices div
+//var start button 
+// submit button for line 25
+//var line 24 intial
+//function 
+
+const startBt = document.getElementById("start-btn");
+
+function Start(){
+    timerEL.innerHTML = time
+timerId = setInterval(countDown, 1000)
+
+//Hide All starter code
+//Show question
+//We do this by using document.getElementByClass (intro)
+}
+
+function countDown(){
+    time--
+
+    timerEL.innerText = time
+    
+ if (time <= 0){
+     stop()
+ }
+
+
+}
+
+function stop(){
+    clearInterval(timerId)
+    console.log('Quiz has Stopped')
+}
+startBt.onclick = Start
--- asset/css/js/question.js
+++ asset/css/js/question.js
@@ -2,32 +2,42 @@ const questionsArr = [
     {
         question: 'Which of the following is true about variable naming conventions in JavaScript?',
         choices: ["A - JavaScript variable names must begin with a letter or the underscore character.",
-        "B - JavaScript variable names are case sensitive.",
-        "C - Both of the above.",
-        "D - None of the above."],
+                "B - JavaScript variable names are case sensitive.",
+                "C - Both of the above.",
+                "D - None of the above."],
         answer: "C - Both of the Above"
     },
     {
         question:'Can you assign a anonymous function to a variable?',
-        choices:["A - true","B - false" ],
+        choices:["A - true",
+                "B - false" ],
         answer:"A - true",
     },
 
     {
         question:'Which built-in method returns the character at the specified index?',
-        choices:[],
-        answer:"",
+        choices:["A - characterAt()",
+                "B - getCharAt()",
+                "C - charAt()",
+                "D - None of the above."],
+        answer:"C - charAt()"
     },
     {
-        question:'',
-        choices:[],
-        answer:"",
+        question:'Which built-in method reverses the order of the elements of an array?',
+        choices:["A - changeOrder(order)",
+                "B - reverse()",   
+                "C - sort(order)",
+                "D - None of the above."],
+        answer:"B - reverse()",
     },
     {
-        question:'',
-        choices:[],
-        answer:"",
+        question:'Which of the following function of Array object adds and/or removes elements from an array?',
+        choices:["A - toSource()",
+                "B - sort()",
+                "C - splice()",
+                "D - unshift()"],
+        answer:"C - splice()",
     }
 
 
-]
+];
